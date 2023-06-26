c9ad92c4147b315ca56e988d559dd220698b5d86
Bernard Sapida <BERNARDSAPIDA1706@GMAIL.COM>
2023-06-26 13:51:05 +0800

    UPDATE: Quality Policy highlight text

--- components/home/QualityPolicy.tsx
+++ components/home/QualityPolicy.tsx
@@ -3,10 +3,13 @@ export default function QualityPolicy() {
     <section className="my-5">
       <h2 className="text-green text-center">Quality Policy</h2>
       <p className="quality-policy text-center">
-        We Commit to the highest standards of education, value our stakeholders,
-        Strive for continual improvement of our products and services, and
-        Uphold the University's tenets of Truth, Excellence, and Service to
-        produce globally competitive and morally upright individuals.
+        We <span className="text-primary">C</span>ommit to the highest standards
+        of education, <span className="text-primary">V</span>alue our
+        stakeholders, <span className="text-primary">S</span>trive for continual
+        improvement of our products and services, and{" "}
+        <span className="text-primary">U</span>phold the University's tenets of
+        Truth, Excellence, and Service to produce globally competitive and
+        morally upright individuals.
       </p>
     </section>
   );
