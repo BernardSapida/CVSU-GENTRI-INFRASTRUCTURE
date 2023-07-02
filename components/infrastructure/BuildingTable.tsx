b6cb4e993ac7c0d1cfcacced9da70071477a410a
Bernard Sapida <BERNARDSAPIDA1706@GMAIL.COM>
2023-07-02 12:41:55 +0800

    UPDATE: Building image name updated

--- components/infrastructure/BuildingTable.tsx
+++ components/infrastructure/BuildingTable.tsx
@@ -12,7 +12,7 @@ export default function Building({
     <>
       <div className="d-flex gap-4 flex-wrap justify-content-center mt-4 mb-5 building-data">
         <Image
-          src={`/images/${category}/ggsames_1.jpg`}
+          src={`/images/${category}/${building["image"]}.jpg`}
           alt={`Photo of ${building["building"]}`}
           width="300"
           height="400"
